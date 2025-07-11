032d9987fa135134cc1f01d912c01772370be6ec
stephen <sxccit@outlook.com>
2025-07-11 11:54:20 +0800

    feat: 添加Java编译插件并支持Java代码编译

---
+++ homework/src/main/java/com/jlm/homtwork/DemoCController.java
@@ -0,0 +1,17 @@
+package com.jlm.homtwork;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/demo")
+public class DemoCController {
+
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello";
+    }
+
+}

64381e2cb744b89223c7a3c46d22fd9f46efb143
stephen <sxccit@outlook.com>
2025-07-12 18:12:03 +0800

    feat: 添加HomeworkPublish实体和数据库配置

--- homework/src/main/java/com/jlm/homework/entity/HomeworkPublish.java
+++ homework/src/main/java/com/jlm/homework/entity/HomeworkPublish.java
@@ -1,13 +1,17 @@
 package com.jlm.homework.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
 
 import java.util.Date;
 
 @Data
+@Entity
 @Table(name = "homework_publish")
 public class HomeworkPublish {
+    @Id
     private Long id;
     /**
      *作业名称
--- homework/src/main/kotlin/com/jlm/homework/config/DatabaseConfig.kt
+++ homework/src/main/kotlin/com/jlm/homework/config/DatabaseConfig.kt
@@ -11,7 +11,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement
  */
 @Configuration
 @EnableJpaRepositories(
-    basePackages = ["com.jlm.homework.repository"]
+    basePackages = ["com.jlm.homework.repository", "com.jlm.homework.mapper"]
 )
 @EntityScan(
     basePackages = ["com.jlm.homework.entity"]

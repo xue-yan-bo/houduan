d361e8c2a7da51a23a92694166df8781aa4488d9
stephen <sxccit@outlook.com>
2025-07-16 09:39:46 +0800

    refactor(StudentFeginClient): 修改FeignClient注解及请求方式

--- homework/src/main/java/com/jlm/homework/feign/StudentFeginClient.java
+++ homework/src/main/java/com/jlm/homework/feign/StudentFeginClient.java
@@ -2,11 +2,10 @@ package com.jlm.homework.feign;
 
 import com.jlm.homework.entity.Student;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
-import java.util.Map;
 
 /*@FeignClient(
         name = "jlm-student",

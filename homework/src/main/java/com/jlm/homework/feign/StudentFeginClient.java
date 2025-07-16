2f9b1aaa3f9b0e451a23a4d9e8aec5923717cc2b
stephen <sxccit@outlook.com>
2025-07-16 09:38:02 +0800

    feat(StudentFeginClient): 添加RequestParam注解和配置类

--- homework/src/main/java/com/jlm/homework/feign/StudentFeginClient.java
+++ homework/src/main/java/com/jlm/homework/feign/StudentFeginClient.java
@@ -2,17 +2,22 @@ package com.jlm.homework.feign;
 
 import com.jlm.homework.entity.Student;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
+import java.util.Map;
 
 /*@FeignClient(
         name = "jlm-student",
         configuration = FeignConfiguration.class
         )*/
-@FeignClient(value = "jlm-student")
+@FeignClient(value = "jlm-student",configuration = FeignConfiguration.class)
 public interface StudentFeginClient {
 
         @GetMapping("/student/student/list")
-        public List<Student> getStudentList(Integer pageNum, Integer pageSize, Long classesId);
+        public List<Student> getStudentList(
+                @RequestParam("pageNum") Integer pageNum,
+                @RequestParam("pageSize") Integer pageSize,
+                @RequestParam("classesId") Long classesId);
 }

a93e39678de36ea2c857c9b796aa04d1687a144b
李伟栋 <lwd3583@126.com>
2026-01-19 12:49:12 +0800

    练习册、作业去掉用户隔离

--- homework/src/main/java/com/jlm/homework/controller/ExerciseBookController.java
+++ homework/src/main/java/com/jlm/homework/controller/ExerciseBookController.java
@@ -81,11 +81,12 @@ public class ExerciseBookController {
         if(request.getSchoolId()!=null){
             currentSchoolId = request.getSchoolId();
         }else {
-            currentSchoolId = userService.getCurrentSchoolIdSafely();
+            currentSchoolId = userService.getCurrentSchoolId();
         }
         ExerciseBookRequest requestWithSchoolId = request.copy();
-        requestWithSchoolId.setSchoolId(currentSchoolId);
-
+        if(currentSchoolId.compareTo(1000L)!=0) {
+            requestWithSchoolId.setSchoolId(currentSchoolId);
+        }
         Page<ExerciseBookEntity> page = exerciseBookService.searchExerciseBooks(requestWithSchoolId);
         Map<String, Object> result = new HashMap<>();
         result.put("total", page.getTotalElements());
@@ -105,10 +106,10 @@ public class ExerciseBookController {
         }
 
         // 租户隔离：检查练习册是否属于当前学校
-        Long currentSchoolId = userService.getCurrentSchoolIdSafely();
+        /*Long currentSchoolId = userService.getCurrentSchoolIdSafely();
         if (!entity.getSchoolId().equals(currentSchoolId)) {
             throw new ResourceNotFoundNewException("练习册不存在，ID: " + id);
-        }
+        }*/
 
         List<ExerciseBookChapter> exerciseBookChapterList = exerciseBookChapterServer.getByExerciseBookId(entity.getId());
         ExerciseBookResponse response = ExerciseBookResponse.from(entity);
@@ -254,10 +255,10 @@ public class ExerciseBookController {
         }
 
         // 租户隔离：检查练习册是否属于当前学校
-        Long currentSchoolId = userService.getCurrentSchoolIdSafely();
+        /*Long currentSchoolId = userService.getCurrentSchoolIdSafely();
         if (!entity.getSchoolId().equals(currentSchoolId)) {
             throw new ResourceNotFoundNewException("练习册不存在，ID: " + id);
-        }
+        }*/
 
         exerciseBookService.deleteById(id);
         return "删除成功";
